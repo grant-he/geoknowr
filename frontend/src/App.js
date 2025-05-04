ccf87cf49dce617ba26a56c6efc17717a3a6e519
Cindy Zhang <cindyzyx9@gmail.com>
2025-05-03 17:19:53 -0700

    fix bugs
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
             const formDataWithField = new FormData();
             formDataWithField.append('image', formData.get('image'));
 
-            const response = await fetch('http://127.0.0.1:8000/api/push/know', {
+            const response = await fetch('/api/push/know', {
                 method: 'POST',
                 body: formDataWithField,
             });
